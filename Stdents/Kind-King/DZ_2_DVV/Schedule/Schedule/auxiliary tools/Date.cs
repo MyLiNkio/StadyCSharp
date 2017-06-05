771b88a0120e676bcb1f82a7e1fd12bcb090398f
kind-king <kind-king@ex.ua>
2017-06-05 18:53:37 +0300

    DZ_2_DVV

---
+++ Stdents/Kind-King/DZ_2_DVV/Schedule/Schedule/auxiliary tools/Date.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Schedule.student;
+using Schedule.discipline;
+using Schedule.teacher;
+using Schedule.auxiliary_tools;
+
+namespace Schedule.auxiliary_tools
+{
+    public class Date
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        public Date(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        public string Month(int month)
+        {
+            switch (month)
+            {
+                case 1:
+                    return "Сiчня";
+                case 2:
+                    return "Лютого";
+                case 3:
+                    return "Березня";
+                case 4:
+                    return "Квiтня";
+                case 5:
+                    return "Травня";
+                case 6:
+                    return "Червня";
+                case 7:
+                    return "Липня";
+                case 8:
+                    return "Серпня";
+                case 9:
+                    return "Вересня";
+                case 10:
+                    return "Жовтня";
+                case 11:
+                    return "Листопада";
+                case 12:
+                    return "Грудня";
+                default: return "Error";
+            }
+        }
+        public void PrintDate(Date date, Format form)
+        {
+            switch (form)
+            {
+                case Format.type_1:
+                    Console.WriteLine("{0}.{1}.{2}", date.day, date.month, date.year);
+                    break;
+                case Format.type_2:
+                    Console.WriteLine("{1}.{0}.{2}", date.day, date.month, date.year);
+                    break;
+                case Format.type_3:
+                    Console.WriteLine("{0}.{1}.{2}", date.day, date.month, date.year % 100 );
+                    break;
+                case Format.type_4:
+                    Console.WriteLine("День: {0}, Мiсяць: {1} Рiк: {2}", date.day, date.month, date.year);
+                    break;
+                case Format.type_5:
+                    Console.WriteLine("{0} {1} {2}", date.day, date.Month(month), date.year);
+                    break;
+            }
+        }
+
+        public string GetDate(Format form)
+        {
+            switch (form)
+            {
+                case Format.type_1:
+                    return (day + "." + month + "." + year);
+                case Format.type_2:
+                    return (month + "." + day + "." + year);
+                case Format.type_3:
+                    return (day + "." + month + "." + year % 100);
+                case Format.type_4:
+                    return ("День: " + day + "Мiсяць: " + month + "Рiк: " + year);
+                case Format.type_5:
+                    return day + " " + Month(month) + " " + year;
+                case Format.type_6:
+                    return day + " " + Month(month) + " " + year % 100;
+                default: return "";
+            }
+        }
+    }
+
+    public enum Format
+    {
+        type_0,
+        type_1,
+        type_2,
+        type_3,
+        type_4,
+        type_5,
+        type_6,
+    }
+
+}
---
+++ Stdents/Kind-King/DZ_2_DVV/Schedule/Schedule/teacher/Teacher.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Schedule.student;
+using Schedule.discipline;
+using Schedule.teacher;
+using Schedule.auxiliary_tools;
+
+namespace Schedule.teacher
+{
+    public class Teacher
+    {
+        private string firstName;
+        private string lastName;
+        private string surname;
+        private Date birthday;
+        //private MobileNumber[] mobileNumbers = new MobileNumber[1];
+        private List<MobileNumber> mobileNumbers = new List<MobileNumber>();
+        private Curs curs;
+        private List<Discipline> disciplines = new List<Discipline>();
+        // private List<Student> students = new List<Student>(); Студенти не у викладача а у дисципліни!
+
+        public void SetCurs(Curs curs)
+        {
+            if (curs >= Curs.postgraduate)
+                this.curs = curs;
+            else Console.WriteLine("Error!");
+            this.curs = curs;
+        }
+
+        public Teacher(string firstName, string surname, string lastName, Date birthday, MobileNumber mobileNumber, Curs curs) : this(firstName, lastName, surname, birthday, mobileNumber)
+        {
+            SetCurs(curs);
+        }
+
+        public Teacher(string firstName, string lastName, string surname, Date birthday, MobileNumber mobileNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.surname = surname;
+            this.birthday = birthday;
+            AddMobile(mobileNumber);
+        }
+
+
+        public void AddDiscipline(Discipline discipline) // Було б непогано зробити ... template
+        {
+            disciplines.Add(discipline);
+        }
+
+        public void RemoveDiscipline(Discipline discipline) // Було б непогано зробити ... template
+        {
+            disciplines.RemoveAt(disciplines.BinarySearch(discipline));
+        }
+
+        public Discipline GetDiscipline(int i)
+        {
+            if (disciplines[i] != null && disciplines.Count() > i)
+                return disciplines[i];
+            else
+                return GetDiscipline(i - 1);
+        }
+
+        public void RewriteMobile(Discipline newDiscipline, Discipline oldDiscipline)
+        {
+            if (disciplines.Contains(oldDiscipline))
+            {
+                disciplines.Insert(disciplines.BinarySearch(oldDiscipline), newDiscipline);
+            }
+        }
+
+
+        //public void AddStudent(Student student) // Було б непогано зробити ... template
+        //{
+        //    students.Add(student);
+        //}
+
+        //public void RemoveStudent(Student student) // Було б непогано зробити ... template
+        //{
+        //    students.RemoveAt(students.BinarySearch(student));
+        //}
+
+        //public Student GetStudent(int i)
+        //{
+        //    if (students[i] != null && students.Count() > i)
+        //        return students[i];
+        //    else
+        //        return GetStudent(i - 1);
+        //}
+
+        //public void RewriteStudent(Student newStudent, Student oldStudent)
+        //{
+        //    if (students.Contains(oldStudent))
+        //    {
+        //        students.Insert(students.BinarySearch(oldStudent), newStudent);
+        //    }
+        //}
+
+
+        //template <class T> не проконало :(
+
+        public void AddMobile(MobileNumber mobileNumber) // Було б непогано зробити ... template
+        {
+            mobileNumbers.Add(mobileNumber);
+        }
+
+        public void RemoveMobile(MobileNumber mobileNumber) // Було б непогано зробити ... template
+        {
+            mobileNumbers.RemoveAt(mobileNumbers.BinarySearch(mobileNumber));
+        }
+
+        public string GetMobileNumber(int i)
+        {
+            if (mobileNumbers[i] != null && mobileNumbers.Count() > i)
+                return mobileNumbers[i].GetMobileNumber();
+            else
+                return GetMobileNumber(i - 1);
+        }
+
+        public void RewriteMobile(MobileNumber newMobileNumber, MobileNumber oldMobileNumber)
+        {
+            if (mobileNumbers.Contains(oldMobileNumber))
+            {
+                mobileNumbers.Insert(mobileNumbers.BinarySearch(oldMobileNumber), newMobileNumber);
+            }
+        }
+
+        public string GetBirthday(Format type)
+        {
+            return birthday.GetDate(type);
+        }
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        public string GetSurname()
+        {
+            return surname;
+        }
+
+        public string GetCurse()
+        {
+            switch (curs)
+            {
+                case Curs.postgraduate:
+                    return "Аспірант";
+                case Curs.practices:
+                    return "Практик";
+                case Curs.lecturer:
+                    return "Лектор";
+                case Curs.professor:
+                    return "Професор";
+                default:
+                    return ""; 
+            }
+        }
+    }
+}
