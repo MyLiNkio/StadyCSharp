2b1109a8922a8f7ad4d71b687bd3db3e150bd16f
Diana <diana_grakova@mail.ru>
2017-06-05 13:15:26 +0300

    DZ2_Diana

---
+++ Stdents/Diana/DZ2_Diana/DZ2_Diana/ListOfProfessors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DZ2_Diana
+{
+    class ListOfProfessors
+    {
+        public ListOfProfessors()
+        {
+            //Professor dyhovychnyi = new Professor( "Dyhovychnyi" , { "Lineyka", "Difur", "Matan" }, lec.Length );
+            //Professor antonets = new Professor("Antonets", { "Vstup", "Dzherela", "RPdP"}, lec.Length);
+            //Professor mogilnyi = new Professor("Mogilnyi", {"DSP", "DigitalDev"}, lec.Length);
+        }
+    }
+}
