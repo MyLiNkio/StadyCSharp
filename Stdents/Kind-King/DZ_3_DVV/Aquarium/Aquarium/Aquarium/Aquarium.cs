c4ed6a844baccdbd175891a2bb4d443ce98382c6
kind-king <kind-king@ex.ua>
2017-06-10 11:17:13 +0300

    DZ_3_DVV

---
+++ Stdents/Kind-King/DZ_3_DVV/Aquarium/Aquarium/Aquarium/Aquarium.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium.Aquarium
+{
+    public class Aquarium
+    {
+        private int length;
+        private int width;
+        private int heigth;
+        private int water;
+
+        public Aquarium(int length, int width, int heigth)
+        {
+            this.length = length;
+            this.width = width;
+            this.heigth = heigth;
+            water = heigth;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int GetHeigth()
+        {
+            return heigth;
+        }
+
+        public int GetWater()
+        {
+            return water;
+        }
+    }
+}
---
+++ Stdents/Kind-King/DZ_3_DVV/Aquarium/Aquarium/Fish/Species/Peaceful.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium.Fish.Species
+{
+    class Peaceful : Fish
+    {
+        public Peaceful(Projection view) : base(view)
+        {
+            velocity = 1.5;
+            reaction = 5;
+            type = FishType.Peaceful;
+            food = null;
+        }
+    }
+}
